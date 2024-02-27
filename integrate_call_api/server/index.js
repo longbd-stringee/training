b189bf8de345fd2d4497023c93883585abd76125
duclong <long.buiduc@ohmidasvn.com>
2024-02-27 22:30:24 +0700

    Commit

---
+++ integrate_call_api/server/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+const port = 3000;
+
+app.use(morgan("common"));
+
+app.get("/project_answer_url", (req, res) => {
+    let { from, to, fromInternal, userId, projectId, custom } = req.query;
+    console.log("🚀 ~ req.query:", req.query);
+    from = decodeURIComponent(from);
+    to = decodeURIComponent(to);
+    custom = decodeURIComponent(custom);
+
+    const toInt = Number(to);
+    const toType = toInt < 60000 ? "internal" : "external";
+
+    const connectAction = {
+        action: "connect",
+        from: {
+            type: "internal",
+            number: from,
+            alias: from,
+        },
+        to: {
+            type: toType,
+            number: to,
+            alias: to,
+        },
+        customData: `custom-data-from-server-${custom}`,
+        timeout: 45,
+        maxConnectTime: -1,
+        peerToPeerCall: false,
+    };
+    console.log("🚀 ~ connectAction:", connectAction);
+
+    return res.json([connectAction]);
+});
+
+app.get("/number_answer_url", (req, res) => {
+    const { userId, from, to } = req.query;
+
+    const callTo = userId ?? "USER_ID";
+
+    const scco = [
+        {
+            action: "connect",
+            from: {
+                type: "external",
+                number: `'${from}'`,
+                alias: `'${from}'`,
+            },
+            to: {
+                type: "internal",
+                number: `'${callTo}'`,
+                alias: `'${to}'`,
+            },
+            customData: "test-custom-data",
+        },
+    ];
+
+    return res.json(scco);
+});
+
+app.listen(port, () => {
+    console.log("App listen on port", port);
+});
