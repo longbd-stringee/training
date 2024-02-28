e271261791765508c0692e6cf6aa9af10957b23a
duclong <long.buiduc@ohmidasvn.com>
2024-02-28 21:30:23 +0700

    Integrate API complete

---
+++ integrate_call_api/client/src/App.jsx
@@ -0,0 +1,236 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { StringeeCall, StringeeClient } from "stringee";
+import axios from "axios";
+
+const hotline = "842471098552";
+const client = new StringeeClient();
+
+const axiosClient = axios.create({
+    baseURL: "http://localhost:8080",
+    headers: {
+        "Content-Type": "Application/JSON",
+    },
+});
+
+axiosClient.interceptors.response.use(
+    function (response) {
+        return response.data;
+    },
+    function (err) {
+        return Promise.reject(err);
+    }
+);
+
+function App() {
+    const [accessToken, setAccessToken] = useState("");
+    const [userId, setUserId] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("84911448457");
+    const [toUserId, setToUserId] = useState("");
+
+    const remoteVideo = useRef(null);
+    const localVideo = useRef(null);
+    const call = useRef(null);
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        window.localStorage.setItem("userId", userId);
+        axiosClient
+            .get(`/access_token?userId=${userId}`)
+            .then((res) => {
+                setTimeout(() => {
+                    setAccessToken(res.data);
+                }, 7000);
+            })
+            .catch(() => {
+                alert("Login failed");
+            });
+    };
+
+    const handleHangUp = useCallback(() => {
+        const userId = window.localStorage.getItem("userId");
+        axiosClient.post("/hang-up", { userId }).then(console.log);
+    }, []);
+
+    const handleDisconnect = useCallback(() => {
+        const userId = window.localStorage.getItem("userId");
+        axiosClient.post("/disconnect", { userId }).then(console.log);
+    }, []);
+
+    const settingCallEvent = useCallback(
+        (call1) => {
+            call1.on("addremotestream", function (stream) {
+                // reset srcObject to work around minor bugs in Chrome and Edge.
+                remoteVideo.current.srcObject = null;
+                remoteVideo.current.srcObject = stream;
+            });
+
+            call1.on("addlocalstream", function (stream) {
+                // reset srcObject to work around minor bugs in Chrome and Edge.
+                console.log("addlocalstream");
+                localVideo.current.srcObject = null;
+                localVideo.current.srcObject = stream;
+            });
+
+            call1.on("signalingstate", function (state) {
+                console.log("signalingstate ", state);
+                const { reason, code } = state;
+                if (code === 5) {
+                    handleHangUp();
+                }
+                document.querySelector("#callStatus").textContent = reason;
+            });
+
+            call1.on("mediastate", function (state) {
+                console.log("mediastate ", state);
+            });
+
+            call1.on("info", function (info) {
+                console.log("on info:" + JSON.stringify(info));
+            });
+        },
+        [handleHangUp]
+    );
+
+    const handleCallAppToPhone = () => {
+        call.current = new StringeeCall(client, hotline, phoneNumber, false);
+
+        settingCallEvent(call.current);
+        call.current.makeCall(function (res) {
+            console.log("make call callback: " + JSON.stringify(res));
+        });
+    };
+
+    const handleCallAppToApp = (videoCall = false) => {
+        call.current = new StringeeCall(client, userId, toUserId, videoCall);
+
+        settingCallEvent(call.current);
+        call.current.makeCall(function (res) {
+            console.log("make call callback: " + JSON.stringify(res));
+        });
+    };
+
+    useEffect(() => {
+        if (accessToken) {
+            client.connect(accessToken);
+        }
+    }, [accessToken]);
+
+    useEffect(() => {
+        client.on("connect", function () {
+            console.log("connected");
+        });
+
+        client.on("authen", function (res) {
+            console.log("authen", res);
+        });
+
+        client.on("disconnect", function () {
+            handleDisconnect();
+        });
+
+        window.onbeforeunload = function () {
+            handleDisconnect();
+        };
+
+        client.on("incomingcall", function (incomingcall) {
+            console.log("incomingcall", incomingcall);
+            call.current = incomingcall;
+            settingCallEvent(incomingcall);
+            var answer = confirm(
+                "Incoming call from: " +
+                    incomingcall.fromNumber +
+                    ", do you want to answer?"
+            );
+            if (answer) {
+                call.current.answer(function (res) {
+                    console.log("answer res", res);
+                });
+            } else {
+                handleHangUp();
+                call.current.reject(function (res) {
+                    console.log("reject res", res);
+                });
+            }
+        });
+    }, [handleDisconnect, handleHangUp, settingCallEvent]);
+
+    if (!accessToken) {
+        return (
+            <form onSubmit={handleLogin}>
+                <div>
+                    <label htmlFor="userId">User id:</label>
+                    <input
+                        id="userId"
+                        type="text"
+                        value={userId}
+                        onChange={(e) => setUserId(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        );
+    }
+
+    return (
+        <div>
+            <center>User id: {userId}</center>
+            <div>
+                <video
+                    id="localVideo"
+                    ref={localVideo}
+                    autoPlay
+                    muted
+                    style={{ width: "150px" }}
+                ></video>
+                <video
+                    id="remoteVideo"
+                    ref={remoteVideo}
+                    autoPlay
+                    style={{ width: "150px" }}
+                ></video>
+                <p id="callStatus"></p>
+            </div>
+            <div>
+                <input
+                    type="text"
+                    id="phone-input"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+                <button
+                    id="call-btn"
+                    disabled={!phoneNumber}
+                    onClick={handleCallAppToPhone}
+                >
+                    Call app - phone
+                </button>
+            </div>
+
+            <div>
+                <input
+                    type="text"
+                    value={toUserId}
+                    onChange={(e) => setToUserId(e.target.value)}
+                />
+                <button
+                    id="call-btn"
+                    disabled={!toUserId}
+                    onClick={() => handleCallAppToApp()}
+                >
+                    Call app - app (Voice)
+                </button>
+                <button
+                    id="call-btn"
+                    disabled={!toUserId}
+                    onClick={() => handleCallAppToApp(true)}
+                >
+                    Call app - app (Video)
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
--- integrate_call_api/server/index.js
+++ integrate_call_api/server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const { getAccessToken } = require("./utils");
 
@@ -7,17 +8,27 @@ const app = express();
 
 const port = 8080;
 
+let userIds = [];
+let busyUserIds = [];
+
+const getAvailableUserId = () => {
+    const ids = userIds.filter((id) => !busyUserIds.includes(id));
+
+    return ids.length ? ids[0] : null;
+};
+
+app.use(cors({ origin: "*" }));
 app.use(morgan("common"));
+app.use(express.json());
 
 app.get("/project_answer_url", (req, res) => {
-    let { from, to, fromInternal, userId, projectId, custom } = req.query;
-    console.log("🚀 ~ req.query:", req.query);
+    let { from, to, userId } = req.query;
     from = decodeURIComponent(from);
     to = decodeURIComponent(to);
-    custom = decodeURIComponent(custom);
 
-    const toInt = Number(to);
-    const toType = toInt < 60000 ? "internal" : "external";
+    const toType = !to.startsWith("84") ? "internal" : "external";
+
+    busyUserIds.push(userId);
 
     const connectAction = {
         action: "connect",
@@ -31,20 +42,40 @@ app.get("/project_answer_url", (req, res) => {
             number: to,
             alias: to,
         },
-        customData: `custom-data-from-server-${custom}`,
-        timeout: 45,
-        maxConnectTime: -1,
-        peerToPeerCall: false,
+        customData: `custom-data-from-server`,
     };
-    console.log("🚀 ~ connectAction:", connectAction);
 
     return res.json([connectAction]);
 });
 
-app.get("/number_answer_url", (req, res) => {
+app.get("/answer_url", (req, res) => {
     const { userId, from, to } = req.query;
 
-    const callTo = userId ?? "USER_ID";
+    let callTo = userId;
+    if (!callTo || busyUserIds.includes(callTo)) {
+        callTo = getAvailableUserId();
+    }
+
+    if (callTo) {
+        busyUserIds.push(callTo);
+    }
+
+    // const scco = [
+    //     {
+    //         action: "connect",
+    //         from: {
+    //             type: "external",
+    //             number: from,
+    //             alias: "842471098552",
+    //         },
+    //         to: {
+    //             type: "external",
+    //             number: "84972105290",
+    //             alias: "84972105290",
+    //         },
+    //         customData: "test-custom-data",
+    //     },
+    // ];
 
     const scco = [
         {
@@ -71,8 +102,31 @@ app.get("/access_token", (req, res) => {
 
     const token = getAccessToken(userId);
 
+    userIds.push(userId);
+
     return res.json({
         data: token,
+    });
+});
+
+app.post("/hang-up", (req, res) => {
+    const { userId } = req.body;
+
+    busyUserIds = busyUserIds.filter((id) => id !== userId);
+
+    return res.json({
+        data: true,
+    });
+});
+
+app.post("/disconnect", (req, res) => {
+    const { userId } = req.body;
+
+    busyUserIds = busyUserIds.filter((id) => id !== userId);
+    userIds = userIds.filter((id) => id !== userId);
+
+    return res.json({
+        data: true,
     });
 });
 
